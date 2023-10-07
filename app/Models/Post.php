8b988c3b3586f4c7242a1b828ccaf6aacaa0319d
iyendoang <ajun190909@gmail.com>
2023-10-08 01:09:57 +0700

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'slug', 'category_id', 'user_id', 'content', 'image'
+    ];
+    /**
+     * category
+     *
+     * @return void
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    /**
+     * user
+     *
+     * @return void
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
+    }
+}
